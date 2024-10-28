db71c13e33c5a595ef23c54d4fd73a1100bdb980
javimt <101430852+javimt@users.noreply.github.com>
2024-10-28 18:44:23 -0500

    models
    Signed-off-by: javimt <101430852+javimt@users.noreply.github.com>

--- plataforma-ventas-nube/src/app/api/DB/models/PaymentPlatform.ts
+++ plataforma-ventas-nube/src/app/api/DB/models/PaymentPlatform.ts
@@ -2,7 +2,7 @@ import { DataTypes, Sequelize, UUIDV4 } from "sequelize";
 import { Database } from "../../interfaces";
 
 const modelInstance = (sequelizeInstance:Sequelize | any) => {
-  const Membership = sequelizeInstance.define('Membership', {
+  const PaymentPlatform = sequelizeInstance.define("PaymentPlatform", {
     id: {
       type: DataTypes.UUID,
       defaultValue: UUIDV4,
@@ -13,22 +13,20 @@ const modelInstance = (sequelizeInstance:Sequelize | any) => {
       type: DataTypes.STRING
     },
 
-    price: {
-      type: DataTypes.FLOAT
-    },
-
     currency: {
-      type: DataTypes.ENUM("ARG", "COP", "USD"),
-      defaultValue: "USD"
+      type:DataTypes.STRING
     },
-  })
 
-  Membership.associate = (models: Database) => {
+    active: {
+      type: DataTypes.BOOLEAN
+    }
+    
+  });
+  PaymentPlatform.associate = (models: Database) => {
     console.log(models)
   }
 
-  return Membership
-
+  return PaymentPlatform;
 }
 
 export default modelInstance;
