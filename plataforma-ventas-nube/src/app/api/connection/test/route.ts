44af20a626105c5aa2073e6edc3a019ccb725870
Nestor novella <importarnorth@gmail.com>
2024-10-30 11:55:09 -0300

    change

--- plataforma-ventas-nube/src/app/api/connection/test/route.ts
+++ plataforma-ventas-nube/src/app/api/connection/test/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
-import sequelize, { checkConection } from "../../DB/db";
+import sequelize from "../../DB/db";
 const {models} = sequelize
 
 
 export async function GET (){
-   
+    const usuarios = await models.User.findAll()
+    return NextResponse.json(usuarios)
+
+}
+
+export async function POST (){
     const usuarios = await models.User.findAll()
     return NextResponse.json(usuarios)
 
