0c67159ef7dda474202d8f1ad25237998a24bb52
Nestor novella <importarnorth@gmail.com>
2024-10-30 12:13:37 -0300

    change

--- plataforma-ventas-nube/src/app/api/connection/test/route.ts
+++ plataforma-ventas-nube/src/app/api/connection/test/route.ts
@@ -1,16 +1,38 @@
-import { NextResponse } from "next/server";
-import sequelize from "../../DB/db";
-const {models} = sequelize
+import { NextRequest, NextResponse } from "next/server";
+import sequelize, { checkConection } from "../../DB/db";
+const { models } = sequelize
 
 
-export async function GET (){
-    const usuarios = await models.User.findAll()
-    return NextResponse.json(usuarios)
+export async function GET() {
+    try {
+        await checkConection()
+        const usuarios = await models.User.findAll()
+        return NextResponse.json({
+            data:usuarios})
+    } catch (error: any) {
+        return NextResponse.json({
+            error: {
+                message: error.message
+            }
+        })
+    }
 
 }
 
-export async function POST (){
-    const usuarios = await models.User.findAll()
-    return NextResponse.json(usuarios)
+export async function POST(request:NextRequest) {
+    await checkConection()
+    
+    try {
+        const body = await request.json()
+        const usuario = await models.User.create(body)
+        if(!usuario) throw new Error('el usuario no fue creado')
+        return NextResponse.json("usuario creado")
+    } catch (error: any) {
+        return NextResponse.json({
+            error: {
+                message: error.message
+            }
+        })
+    }
 
 }
