e864832ee4e7d041a0c6fbaa54cfb39b152f4b3b
Nestor novella <importarnorth@gmail.com>
2024-11-08 11:52:44 -0300

    changes

--- plataforma-ventas-nube/src/app/[store]/page.tsx
+++ plataforma-ventas-nube/src/app/[store]/page.tsx
@@ -1,15 +1,19 @@
 
 //www.tienda-nube/electonicaDigital
-
+"use server"
 import { getStore } from "./getStore";
 
 async function Main({params}:{params:{store:string}}) {
 const {store} = params
      const tienda =await getStore(store)
-     console.log(tienda)
+     console.log(tienda.data.ConfigurationStore.banner.color)
+     const colors = {
+        red: 'text-red-500',
+        blue: 'text-blue-300'
+     }
     return ( 
         <div className="p-2 w-full flex justify-center">
-            <h2 className="text-[50px] font-semibold text-blue-400">{tienda.data.name}</h2>
+            <h2 className={`text-[50px] font-semibold  ${colors[tienda.data.ConfigurationStore.banner.color]}`}>{tienda.data.name}</h2>
         </div>
      );
 }
